9ca35be0769a0c102b19f86fd708c2acb815e24c
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-05 18:19:08 +0300

    AddTeam and SortTeams methods implementation

--- FootballRanking/FootballRanking/Standings.cs
+++ FootballRanking/FootballRanking/Standings.cs
@@ -1,18 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FootballRanking
 {
-    class Standings
+    public static class Standings
     {
-        private Team[] ranking;
-        int index = 0;
+        public static List<KeyValuePair<string, int>> teams = new List<KeyValuePair<string, int>>();
 
-        public void AddTeam(string name, int points)
+        public static void AddTeam(string name, int points)
         {
-            ranking[index] = new Team(name, points);
-            index++;
+            KeyValuePair<string, int> team = new KeyValuePair<string, int>(name, points);
+            teams.Add(team);
+        }
+
+        public static void SortTeams()
+        {
+            SortTeams(teams, 0, teams.Count - 1);
+        }
+
+        private static void SortTeams(List<KeyValuePair<string, int>> teams, int start, int end)
+        {
+            int pivot = 0;
+            if (start >= end)
+            {
+                return;
+            }
+
+            pivot = Partition(teams, start, end);
+            SortTeams(teams, start, pivot - 1);
+            SortTeams(teams, pivot + 1, end);
+        }
+
+        private static int Partition(List<KeyValuePair<string, int>> teams, int start, int end)
+        {
+            int pivot = teams.ElementAt(end).Value;
+            int i = start - 1;
+
+            for (int j = start; j < end; j++)
+            {
+                if (teams.ElementAt(j).Value > pivot)
+                {
+                    i++;
+                    KeyValuePair<string, int> temp = teams[i];
+                    teams[i] = teams[j];
+                    teams[j] = temp;
+                }
+            }
+
+            KeyValuePair<string, int> temp2 = teams.ElementAt(i + 1);
+            teams[i + 1] = teams[end];
+            teams[end] = temp2;
+
+            return i + 1;
         }
     }
 }
--- FootballRanking/FootballRanking/StandingsFacts.cs
+++ FootballRanking/FootballRanking/StandingsFacts.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
+using static FootballRanking.Standings;
 
 namespace FootballRanking.Facts
 {
-    class StandingsFacts
+    public class StandingsFacts
     {
+        [Fact]
+        public void AddTeamToStandings()
+        {
+            AddTeam("FCSB", 33);
+            AddTeam("Viitorul", 40);
+            KeyValuePair<string, int> teamToCheck = new KeyValuePair<string, int>("FCSB", 33);
+            Assert.True(teams[0].Key == teamToCheck.Key && teams[0].Value == teamToCheck.Value);
+        }
+
+        [Fact]
+        public void SortTeamsByPoints()
+        {
+            SortTeams();
+            Assert.True(teams[0].Value > teams[1].Value);
+        }
     }
 }
