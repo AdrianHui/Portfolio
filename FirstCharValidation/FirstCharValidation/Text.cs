55521b68095af89eadfc6e9ee512ca7c8aa9e42c
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-22 07:44:11 +0300

    Text and TextFacts classes added

---
+++ FirstCharValidation/FirstCharValidation/Text.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringValidation
+{
+    class Text
+    {
+        string prefix;
+        
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new FailedMatch(text);
+            }
+
+            if (prefix == "")
+            {
+                return new SuccessMatch(text);
+            }
+
+            bool result = true;
+            foreach (var character in text)
+            {
+                IMatch match = new Character(character).Match(prefix);
+                if (!match.Success())
+                {
+                    result = false;
+                    break;
+                }
+
+                prefix = prefix.Substring(1);
+            }
+
+            return result ? new SuccessMatch(prefix) : (IMatch) new FailedMatch(text);
+        }
+    }
+}
---
+++ FirstCharValidation/FirstCharValidation/TextFacts.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+
+namespace StringValidation.Facts
+{
+    public class TextFacts
+    {
+        [Fact]
+        public void InputStringIsNullAndPrefixContainsTextShouldReturnFalseAndRemainingTextShouldReturnNull()
+        {
+            Text text = new Text("abc");
+            IMatch match = text.Match(null);
+            Assert.False(match.Success());
+            Assert.True(match.RemainingText() == null);
+        }
+
+        [Fact]
+        public void InputStringIsEmptyStringShouldReturnFalseAndRemainingTextShouldReturnEmptyString()
+        {
+            Text text = new Text("abc");
+            IMatch match = text.Match("");
+            Assert.False(match.Success());
+            Assert.True(match.RemainingText() == "");
+        }
+
+        [Fact]
+        public void InputStringIsNullAndPrefixDoesntContainsTextShouldReturnTrueAndRemainingTextShouldReturnTheText()
+        {
+            Text text = new Text("");
+            IMatch match = text.Match("abc");
+            Assert.True(match.Success() && match.RemainingText() == "abc");
+        }
+        
+        [Fact]
+        public void InputStringFullyMatchesPrefixShouldRetrunTrueAndRemainingTextShouldReturnEmptyString()
+        {
+            Text text = new Text("abCd");
+            IMatch match = text.Match("abCd");
+            Assert.True(match.Success() && match.RemainingText() == "");
+        }
+
+        [Fact]
+        public void InputStringPartialyMatchesPrefixShouldRetrunTrueAndRemainingTextShouldReturnRemainingPrefix()
+        {
+            Text text = new Text("abCdEf");
+            IMatch match = text.Match("abCd");
+            Assert.True(match.Success() && match.RemainingText() == "Ef");
+        }
+
+        [Fact]
+        public void InputStringDoesntMatchPrefixShouldRetrunFalseAndRemainingTextShouldReturnRemainingTheText()
+        {
+            Text text = new Text("abCdEf");
+            IMatch match = text.Match("ghi");
+            Assert.False(match.Success());
+            Assert.True(match.RemainingText() == "ghi");
+        }
+    }
+}
