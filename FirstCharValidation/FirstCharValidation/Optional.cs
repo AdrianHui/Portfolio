11c48ce81306b22eb50f240a012ee669fecf718c
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-22 18:34:26 +0300

    Optional class added

---
+++ FirstCharValidation/FirstCharValidation/Optional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringValidation
+{
+    class Optional : IPattern
+    {
+        IPattern pattern;
+
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = pattern.Match(text);
+            if (match.Success())
+            {
+                return new SuccessMatch(text.Substring(1));
+            }
+
+            return new SuccessMatch(text);
+        }
+    }
+}
