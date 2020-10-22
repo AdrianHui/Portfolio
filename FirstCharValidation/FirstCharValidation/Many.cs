61046c77bf3d09af365ea053e0f489c85bb0b02f
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-22 17:48:36 +0300

    Many class added

---
+++ FirstCharValidation/FirstCharValidation/Many.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringValidation
+{
+    class Many : IPattern
+    {
+        IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new SuccessMatch(text);
+            }
+
+            IMatch match = new SuccessMatch(text);
+            foreach (var charactrer in text)
+            {
+                match = pattern.Match(match.RemainingText());
+                if (!match.Success())
+                {
+                    return new SuccessMatch(match.RemainingText());
+                }
+            }
+
+            return new SuccessMatch(text);
+        }
+    }
+}
---
+++ FirstCharValidation/FirstCharValidation/ManyFacts.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace StringValidation.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void InputStringIsNullShouldReturnTrueAndNull()
+        {
+            var pattern = new Many(new Character('a'));
+            Assert.True(pattern.Match(null).Success()
+                    && pattern.Match(null).RemainingText() == null);
+        }
+
+        [Fact]
+        public void InputStringIsEmptyStringShouldReturnTrueAndEmptyString()
+        {
+            var pattern = new Many(new Character('a'));
+            Assert.True(pattern.Match("").Success()
+                    && pattern.Match("").RemainingText() == "");
+        }
+
+        [Fact]
+        public void InputStringContainsPatternMultipleTimesShouldReturnTrueAndRemainingText()
+        {
+            var pattern = new Many(new Character('a'));
+            Assert.True(pattern.Match("aaaabc").Success()
+                    && pattern.Match("aaaabc").RemainingText() == "bc");
+        }
+
+        [Fact]
+        public void InputStringDoesntContainPatternShouldReturnTrueAndInputString()
+        {
+            var pattern = new Many(new Character('a'));
+            Assert.True(pattern.Match("bc").Success()
+                    && pattern.Match("bc").RemainingText() == "bc");
+        }
+
+        [Fact]
+        public void InputStringContainsCharsInPatternRangeShouldReturnTrueAndRemainingText()
+        {
+            var pattern = new Many(new Range('0', '9'));
+            Assert.True(pattern.Match("123ab1234").Success()
+                    && pattern.Match("123ab1234").RemainingText() == "ab1234");
+        }
+
+        [Fact]
+        public void InputStringDoesntContainCharsInPatternRangeShouldReturnTrueAndInputString()
+        {
+            var pattern = new Many(new Range('0', '9'));
+            Assert.True(pattern.Match("abc").Success()
+                    && pattern.Match("abc").RemainingText() == "abc");
+        }
+    }
+}
