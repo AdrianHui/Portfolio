25ddde0876c3753c51a869be25a2527a21d58261
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-22 06:44:10 +0300

    SuccessMatchFacts and FailedMatchFacts classes added

---
+++ FirstCharValidation/FirstCharValidation/FailedMatchFacts.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace StringValidation.Facts
+{
+    public class FailedMatchFacts
+    {
+        [Fact]
+        public void InputTextIsNullRemainingTextMethodShouldReturnNull()
+        {
+            IMatch test = new FailedMatch(null);
+            Assert.True(test.RemainingText() == null);
+        }
+
+        [Fact]
+        public void InputTextIsEmptyStringRemainingTextMethodShouldReturnEmptyString()
+        {
+            IMatch test = new FailedMatch("");
+            Assert.True(test.RemainingText() == "");
+        }
+
+        [Fact]
+        public void InputTextIsTextRemainingTextMethodShouldReturnTheText()
+        {
+            IMatch test = new FailedMatch("abcd");
+            Assert.True(test.RemainingText() == "abcd");
+        }
+
+        [Fact]
+        public void SuccessMethodShouldReturnFalse()
+        {
+            IMatch test = new FailedMatch("abcd");
+            Assert.False(test.Success());
+        }
+    }
+}
---
+++ FirstCharValidation/FirstCharValidation/SuccessMatchFacts.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace StringValidation.Facts
+{
+    public class SuccessMatchFacts
+    {
+        [Fact]
+        public void InputTextIsNullRemainingTextMethodShouldReturnNull()
+        {
+            IMatch test = new SuccessMatch(null);
+            Assert.True(test.RemainingText() == null);
+        }
+
+        [Fact]
+        public void InputTextIsEmptyStringRemainingTextMethodShouldReturnEmptyString()
+        {
+            IMatch test = new SuccessMatch("");
+            Assert.True(test.RemainingText() == "");
+        }
+
+        [Fact]
+        public void InputTextIsTextRemainingTextMethodShouldReturnTheText()
+        {
+            IMatch test = new SuccessMatch("abcd");
+            Assert.True(test.RemainingText() == "abcd");
+        }
+
+        [Fact]
+        public void SuccessMethodShouldReturnTrue()
+        {
+            IMatch test = new SuccessMatch("abcd");
+            Assert.True(test.Success());
+        }
+    }
+}
