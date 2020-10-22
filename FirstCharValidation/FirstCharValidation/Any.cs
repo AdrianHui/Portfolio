79f54b2d4c01a613b7cb1b2f1483dba12c6e23e7
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-10-22 17:49:07 +0300

    changed to implement IPattern

--- FirstCharValidation/FirstCharValidation/Any.cs
+++ FirstCharValidation/FirstCharValidation/Any.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StringValidation
 {
-    class Any
+    class Any : IPattern
     {
         string accepted;
 
--- FirstCharValidation/FirstCharValidation/Text.cs
+++ FirstCharValidation/FirstCharValidation/Text.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StringValidation
 {
-    class Text
+    class Text : IPattern
     {
         string prefix;
         
