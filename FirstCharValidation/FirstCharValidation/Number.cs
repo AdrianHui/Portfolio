f787f1f438511663cc0db949f99bfc932d9f4eb5
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-04 06:51:22 +0200

    Constructor pattern built and facts

--- FirstCharValidation/FirstCharValidation/Number.cs
+++ FirstCharValidation/FirstCharValidation/Number.cs
@@ -10,7 +10,16 @@ namespace StringValidation
 
         public Number()
         {
-            pattern = new OneOrMore(new Range('0', '9'));
+            var oneNine = new Range('1', '9');
+            var digit = new Choice(new Character('0'), oneNine);
+            var sign = new Any("+-");
+            var exponent = new Any("eE");
+            var fraction = new Sequence(new Character('.'), new Optional(exponent), new OneOrMore(digit));
+            pattern = new Sequence(new Optional(sign),
+                                    new Choice(new Character('0'), new OneOrMore(digit)),
+                                    new Optional(fraction),
+                                    new Optional(new Sequence(exponent, new Optional(sign),
+                                                new OneOrMore(digit))));
         }
 
         public IMatch Match(string text)
--- FirstCharValidation/FirstCharValidation/NumberFacts.cs
+++ FirstCharValidation/FirstCharValidation/NumberFacts.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Reflection.Metadata;
+using Xunit;
 
 namespace StringValidation.Facts
 {
@@ -7,7 +8,7 @@ namespace StringValidation.Facts
         [Fact]
         public void CanBeZero()
         {
-            var pattern = new Choice(new OneOrMore(new Range('1', '9')));
+            var pattern = new Number();
             Assert.True(pattern.Match("0").Success());
             Assert.True(pattern.Match("0").RemainingText() == "");
         }
@@ -15,7 +16,7 @@ namespace StringValidation.Facts
         [Fact]
         public void DoesntContainsLetters()
         {
-            var pattern = new OneOrMore(new Range('0', '9'));
+            var pattern = new Number();
             Assert.False(pattern.Match("b").Success());
             Assert.True(pattern.Match("b").RemainingText() == "b");
         }
@@ -23,7 +24,7 @@ namespace StringValidation.Facts
         [Fact]
         public void CanBeSingleDigit()
         {
-            var pattern = new OneOrMore(new Range('0', '9'));
+            var pattern = new Number();
             Assert.True(pattern.Match("2").Success());
             Assert.True(pattern.Match("2").RemainingText() == "");
         }
@@ -31,7 +32,7 @@ namespace StringValidation.Facts
         [Fact]
         public void CanHaveMultipleDigits()
         {
-            var pattern = new OneOrMore(new Range('0', '9'));
+            var pattern = new Number();
             Assert.True(pattern.Match("21").Success());
             Assert.True(pattern.Match("21").RemainingText() == "");
         }
@@ -39,7 +40,7 @@ namespace StringValidation.Facts
         [Fact]
         public void CantBeNull()
         {
-            var pattern = new OneOrMore(new Range('0', '9'));
+            var pattern = new Number();
             Assert.False(pattern.Match(null).Success());
             Assert.True(pattern.Match(null).RemainingText() == null);
         }
@@ -47,7 +48,7 @@ namespace StringValidation.Facts
         [Fact]
         public void CantBeEmpty()
         {
-            var pattern = new OneOrMore(new Range('0', '9'));
+            var pattern = new Number();
             Assert.False(pattern.Match("").Success());
             Assert.True(pattern.Match("").RemainingText() == "");
         }
@@ -55,8 +56,147 @@ namespace StringValidation.Facts
         [Fact]
         public void CantStartWithZero()
         {
-            var pattern = new Choice(new Sequence(new Character('0'), new Text(string.Empty)), new OneOrMore(new Range('1', '9')));
-            Assert.False(pattern.Match("01").Success());
+            var pattern = new Number();
+            Assert.True(pattern.Match("01").Success());
+            Assert.True(pattern.Match("01").RemainingText() == "1");
+        }
+
+        [Fact]
+        public void CanBeNegative()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("-12").Success());
+            Assert.True(pattern.Match("-12").RemainingText() == "");
+        }
+
+        [Fact]
+        public void CanBeMinusZero()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("-0").Success());
+            Assert.True(pattern.Match("-0").RemainingText() == "");
+        }
+
+        [Fact]
+        public void CanBeFractional()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14").Success());
+            Assert.True(pattern.Match("3.14").RemainingText() == "");
+        }
+
+        [Fact]
+        public void FractionCanHaveLeadingZeros()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("0.00001").Success());
+            Assert.True(pattern.Match("0.00001").RemainingText() == "");
+            Assert.True(pattern.Match("10.00001").Success());
+            Assert.True(pattern.Match("10.00001").RemainingText() == "");
+        }
+
+        [Fact]
+        public void DoesNotEndWithDot()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.").Success());
+            Assert.True(pattern.Match("3.").RemainingText() == ".");
+        }
+
+        [Fact]
+        public void DoesNotHaveTwoFractionParts()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14.12").Success());
+            Assert.True(pattern.Match("3.14.12").RemainingText() == ".12");
+        }
+
+        [Fact]
+        public void DecimalPartDoesNotAcceptLetters()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14p").Success());
+            Assert.True(pattern.Match("3.14p").RemainingText() == "p");
+        }
+
+        [Fact]
+        public void CanHaveExponent()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14e3").Success());
+            Assert.True(pattern.Match("14e3").RemainingText() == "");
+            Assert.True(pattern.Match("14E3").Success());
+            Assert.True(pattern.Match("14E3").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ExponentCanBePositive()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14e+3").Success());
+            Assert.True(pattern.Match("14e+3").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ExponentCanBeNegative()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14e-3").Success());
+            Assert.True(pattern.Match("14e-3").RemainingText() == "");
+        }
+
+        [Fact]
+        public void CanHaveFractionAndExponent()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14e3").Success());
+            Assert.True(pattern.Match("3.14e3").RemainingText() == "");
+        }
+
+        [Fact]
+        public void ExponentMustBeComplete()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14e+").Success());
+            Assert.True(pattern.Match("3.14e+").RemainingText() == "e+");
+            Assert.True(pattern.Match("14e").Success());
+            Assert.True(pattern.Match("14e").RemainingText() == "e");
+            Assert.True(pattern.Match("14e+").Success());
+            Assert.True(pattern.Match("14e+").RemainingText() == "e+");
+            Assert.True(pattern.Match("14e-").Success());
+            Assert.True(pattern.Match("14e-").RemainingText() == "e-");
+        }
+
+        [Fact]
+        public void ExponentDoesNotAllowLetters()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("3.14e+2x2").Success());
+            Assert.True(pattern.Match("3.14e+2x2").RemainingText() == "x2");
+        }
+
+        [Fact]
+        public void DoesNotHaveTwoExponents()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14e23E2").Success());
+            Assert.True(pattern.Match("14e23E2").RemainingText() == "E2");
+        }
+
+        [Fact]
+        public void ExponentIsAfterTheFraction()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14e2.345").Success());
+            Assert.True(pattern.Match("14e2.345").RemainingText() == ".345");
+        }
+
+        [Fact]
+        public void SignCanBeOnlyAfterExponent()
+        {
+            var pattern = new Number();
+            Assert.True(pattern.Match("14.-3e45").Success());
+            Assert.True(pattern.Match("14.-3e45").RemainingText() == ".-3e45");
         }
     }
 }
