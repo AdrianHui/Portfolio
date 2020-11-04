ae6dbc2bf1944dd2532ea82d92298a84f3d636da
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-04 17:34:09 +0200

    improvements

--- FirstCharValidation/FirstCharValidation/Number.cs
+++ FirstCharValidation/FirstCharValidation/Number.cs
@@ -10,16 +10,15 @@ namespace StringValidation
 
         public Number()
         {
-            var oneNine = new Range('1', '9');
-            var digit = new Choice(new Character('0'), oneNine);
+            var digit = new Choice(new Range('0', '9'));
+            var digits = new OneOrMore(digit);
             var sign = new Any("+-");
             var exponent = new Any("eE");
-            var fraction = new Sequence(new Character('.'), new Optional(exponent), new OneOrMore(digit));
-            pattern = new Sequence(new Optional(sign),
-                                    new Choice(new Character('0'), new OneOrMore(digit)),
-                                    new Optional(fraction),
-                                    new Optional(new Sequence(exponent, new Optional(sign),
-                                                new OneOrMore(digit))));
+            var fraction = new Sequence(new Character('.'), new Optional(exponent), digits);
+            var startsWithZero = new Choice(new Character('0'), digits);
+            var decimalPart = new Sequence(exponent, new Optional(sign), digits);
+            pattern = new Sequence(new Optional(sign), startsWithZero,
+                                    new Optional(fraction), new Optional(decimalPart));
         }
 
         public IMatch Match(string text)
