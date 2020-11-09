bd9d2b81cc9df5a78a35bef395c2244df5e20ad4
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-09 18:36:11 +0200

    refactorization

--- FirstCharValidation/FirstCharValidation/String.cs
+++ FirstCharValidation/FirstCharValidation/String.cs
@@ -13,10 +13,10 @@ namespace StringValidation
             var hex = new Choice(new Range('0', '9'), new Range('A', 'F'), new Range('a', 'f'));
             var escape = new Choice(new Sequence(new Character('u'), hex, hex, hex, hex),
                                     new Any("bfnrt/\"\\"));
-            var character = new Range(' ', char.MaxValue, "\"\\");
-            var characters = new OneOrMore(new Choice(character,
-                                                      new Sequence(new Character('\\'), escape)));
-            pattern = new Sequence(new Character('"'), new Optional(characters), new Character('"'));
+            var character = new Choice(new Range(' ', char.MaxValue, "\"\\"),
+                                       new Sequence(new Character('\\'), escape));
+            var characters = new Many(character);
+            pattern = new Sequence(new Character('"'), characters, new Character('"'));
         }
 
         public IMatch Match(string text)
