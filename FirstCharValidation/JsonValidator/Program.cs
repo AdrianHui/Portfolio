b6a176146b8e800d934dd0f7fe72fbb21b927957
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-10 13:02:02 +0200

    console app created to validate a json document

---
+++ FirstCharValidation/JsonValidator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StringValidation;
+
+namespace JsonValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = new Value();
+            string text = System.IO.File.ReadAllText(@"C:\Users\Adrian\Desktop\json1.txt");
+
+            if (pattern.Match(text).Success())
+            {
+                Console.WriteLine("Text is a valid JSON text.");
+            }
+            else
+            {
+                Console.WriteLine("Text is not a valid JSON text.");
+            }
+        }
+    }
+}
