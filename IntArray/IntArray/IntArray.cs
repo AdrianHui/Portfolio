c3c27820a385206522b3ce1e47426bf7bf5686ae
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-15 18:36:36 +0200

    IntArray methods implementation with facts

---
+++ IntArray/IntArray/IntArray.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace IntegerArray
+{
+    class IntArray
+    {
+		public int count;
+		public int[] data;
+		public IntArray()
+		{
+			this.data = new int[4];
+			this.count = 0;
+		}
+
+		public void Add(int element)
+		{
+			count++;
+			Array.Resize(ref data, count);
+			data[count - 1] = element;
+		}
+
+		public int Count()
+		{
+			return count;
+		}
+
+		public int Element(int index)
+		{
+			return data[index];
+		}
+
+		public void SetElement(int index, int element)
+		{
+			var temp = new int[count];
+			for (int i = 0; i < count; i++)
+            {
+				if (i == index)
+                {
+					temp[i] = element;
+                }
+                else
+                {
+					temp[i] = data[i];
+				}
+            }
+
+			data = temp;
+		}
+
+		public bool Contains(int element)
+		{
+			for (int i = 0; i < count; i++)
+            {
+				if (data[i] == element)
+                {
+					return true;
+                }
+            }
+
+			return false;
+		}
+
+		public int IndexOf(int element)
+		{
+			for (int i = 0; i < count; i++)
+            {
+				if (data[i] == element)
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public void Insert(int index, int element)
+		{
+			count++;
+			var temp = new int[count];
+			for (int i = 0; i < count; i++)
+			{
+				if (index > i)
+                {
+					temp[i] = data[i];
+                }
+				else if (index == i)
+                {
+					temp[i] = element;
+                }
+                else
+                {
+					temp[i] = data[i - 1];
+                }
+			}
+
+			data = temp;
+		}
+
+		public void Clear()
+		{
+			data = new int[0];
+			count = 0;
+		}
+
+		public void Remove(int element)
+		{
+			count--;
+			var temp = new int[count];
+			for (int i = 0; i < count + 1; i++)
+			{
+				if (IndexOf(element) > i)
+				{
+					temp[i] = data[i];
+				}
+
+				if (IndexOf(element) < i)
+				{
+					temp[i - 1] = data[i];
+				}
+			}
+
+			data = temp;
+		}
+
+		public void RemoveAt(int index)
+		{
+			count--;
+			var temp = new int[count];
+			for (int i = 0; i < count + 1; i++)
+			{
+				if (i < index)
+                {
+					temp[i] = data[i];
+				}
+
+				if (i > index)
+				{
+					temp[i - 1] = data[i];
+				}
+			}
+
+			data = temp;
+		}
+	}
+}
---
+++ IntArray/IntArray/IntArrayFacts.cs
@@ -0,0 +1,125 @@
+﻿using Xunit;
+
+namespace IntegerArray.Facts
+{
+    public class IntegerArrayFacts
+    {
+        [Fact]
+        public void Add_ShouldAddTheIntToTheEndOfTheArray()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.data[0] == 5 && arr.data[1] == 2);
+        }
+
+        [Fact]
+        public void Count_ShouldReturnNumberOfElementsInArray()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.Count() == 2);
+        }
+
+        [Fact]
+        public void Element_ShouldReturnElementAtGivenIndex()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.Element(1) == 2);
+        }
+
+        [Fact]
+        public void SetElement_ShouldChangeElementValueAtGivenIndex()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            arr.SetElement(0, 3);
+            Assert.True(arr.data[0] == 3);
+        }
+
+        [Fact]
+        public void Contains_ShouldReturnTrueIfGivenElementIsFoundInArray()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.Contains(2));
+        }
+
+        [Fact]
+        public void Contains_ShouldReturnFalseIfGivenElementIsNotFoundInArray()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.False(arr.Contains(3));
+        }
+
+        [Fact]
+        public void IndexOf_ShouldReturnIndexPositionOfGivenElementIfElementIsFound()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.IndexOf(2) == 1);
+        }
+
+        [Fact]
+        public void IndexOf_ShouldReturnNegativeOneIfElementIsNotFound()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            Assert.True(arr.IndexOf(3) == -1);
+        }
+
+        [Fact]
+        public void Insert_ShouldInsertGivenElementAtGivenPosition()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            arr.Insert(1, 3);
+            Assert.True(arr.data[1] == 3 && arr.data[2] == 2);
+        }
+
+        [Fact]
+        public void Clear_ShouldDeleteAllElements()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            arr.Insert(1, 3);
+            arr.Clear();
+            Assert.True(arr.count == 0 && arr.data.Length == 0);
+        }
+
+        [Fact]
+        public void Remove_ShouldRemoveFirstOccurenceOfGivenElement()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(2);
+            arr.Remove(2);
+            Assert.True(arr.data[0] == 5 && arr.data[1] == 3 && arr.data[2] == 2);
+        }
+
+        [Fact]
+        public void RemoveAt_ShouldRemoveElementAtGivenIndex()
+        {
+            var arr = new IntArray();
+            arr.Add(5);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(2);
+            arr.RemoveAt(2);
+            Assert.True(arr.data[0] == 5 && arr.data[1] == 2 && arr.data[2] == 2);
+        }
+    }
+}
