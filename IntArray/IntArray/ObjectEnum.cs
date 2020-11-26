ddaa2a02a58f5d73126275e0d8cbd6eeafa66a40
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-26 06:52:16 +0200

    count replaced with position, MoveNext using objects.Count instead of .Length

---
+++ IntArray/IntArray/ObjectEnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace IntegerArray
+{
+    class ObjectEnum : IEnumerator
+    {
+        private readonly ObjectArray objects;
+        private int position = -1;
+
+        public ObjectEnum(ObjectArray objects)
+        {
+            this.objects = objects;
+        }
+
+        public object Current => objects[position];
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < objects.Count;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
