a15b7216326758421133879fd6c09192b655ebf6
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-11-26 08:21:12 +0200

    List<T> implementation

---
+++ IntArray/IntArray/List.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntegerArray
+{
+    class List<T> : IEnumerable<T>
+    {
+        private T[] data;
+
+        public List()
+        {
+            data = new T[4];
+        }
+
+        public int Count { get; private set; }
+
+        public virtual T this[int index] { get => data[index]; set => data[index] = value; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Add(T element)
+        {
+            ExpandCheck();
+            data[Count] = element;
+            Count++;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) >= 0;
+        }
+
+        public int IndexOf(T element)
+        {
+            return GetIndex(element);
+        }
+
+        public virtual void Insert(int index, T element)
+        {
+            ShiftRight(index);
+            data[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+        }
+
+        public void Remove(T element)
+        {
+            RemoveAt(IndexOf(element));
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        private void ExpandCheck()
+        {
+            if (Count != data.Length)
+            {
+                return;
+            }
+
+            Array.Resize(ref data, data.Length * 2);
+        }
+
+        private void ShiftRight(int stopIndex)
+        {
+            for (int i = Count; i > stopIndex; i--)
+            {
+                ExpandCheck();
+                data[i] = data[i - 1];
+            }
+        }
+
+        private void ShiftLeft(int startIndex)
+        {
+            for (int i = startIndex; i < Count; i++)
+            {
+                data[i] = i == Count - 1 ? default : data[i + 1];
+            }
+        }
+
+        private int GetIndex(T element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (data[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ IntArray/IntArray/ListFacts.cs
@@ -0,0 +1,130 @@
+﻿using Xunit;
+
+namespace IntegerArray.Facts
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void CountShouldReturnNumberOfElements()
+        {
+            var objArray = new List<object>() { 123 };
+            Assert.True(objArray.Count == 1);
+        }
+
+        [Fact]
+        public void ShouldReturnElementAtGivenIndexPosition()
+        {
+            var objArray = new List<object>() { 123 };
+            Assert.True(objArray[0].Equals(123));
+        }
+
+        [Fact]
+        public void ShouldSetElementAtGivenIndexPosition()
+        {
+            var objArray = new List<object>() { 123 };
+            objArray[0] = true;
+            Assert.True(objArray[0].Equals(true));
+        }
+
+        [Fact]
+        public void AddMethodShouldAllowAddingBasicTypes()
+        {
+            var objArray = new List<object>();
+            objArray.Add(123);
+            objArray.Add(true);
+            objArray.Add("abc");
+            objArray.Add('a');
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(true)
+                     && objArray[2].Equals("abc") && objArray[3].Equals('a'));
+        }
+
+        [Fact]
+        public void AddMethodShouldAllowAddingOtherObjectInstances()
+        {
+            var objArray = new List<object>() { 123, true };
+            var intArray = new IntArray();
+            var sortedIntArray = new SortedIntArray();
+            objArray.Add(intArray);
+            objArray.Add(sortedIntArray);
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(true)
+                     && objArray[2].Equals(intArray) && objArray[3].Equals(sortedIntArray));
+        }
+
+        [Fact]
+        public void ContainsMethodShouldReturnTrueIfElementIsInArray()
+        {
+            var objArray = new List<object>() { 123, true };
+            Assert.True(objArray.Contains(123));
+        }
+
+        [Fact]
+        public void ContainsMethodShouldReturnFalseIfElementIsNotInArray()
+        {
+            var objArray = new List<object>() { 123 };
+            Assert.False(objArray.Contains(true));
+        }
+
+        [Fact]
+        public void IndexOfMethodShouldReturnElementIndexIfIsInArray()
+        {
+            var objArray = new List<object>() { 123, true };
+            Assert.True(objArray.IndexOf(123) == 0);
+        }
+
+        [Fact]
+        public void IndexOfMethodShouldReturnNegativeOneIfElementIsNotInArray()
+        {
+            var objArray = new List<object>() { 123 };
+            Assert.True(objArray.IndexOf(true) == -1);
+        }
+
+        [Fact]
+        public void InsertMethodShouldInsertGivenElementAtGivenPosition()
+        {
+            object intArray = new IntArray();
+            var objArray = new List<object>() { 123, true };
+            objArray.Insert(1, intArray);
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(intArray)
+                     && objArray[2].Equals(true));
+        }
+
+        [Fact]
+        public void InsertMethodShouldInsertGivenElementAtGivenPositionEvenIfAllPositionsAreFull()
+        {
+            object intArray = new IntArray();
+            var objArray = new List<object>() { 123, true, "abc", 'a' };
+            objArray.Insert(1, intArray);
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(intArray)
+                     && objArray[2].Equals(true) && objArray[4].Equals('a'));
+        }
+
+        [Fact]
+        public void ClearMethodShouldResetElementCount()
+        {
+            var objArray = new List<object>() { 123, true };
+            objArray.Clear();
+            Assert.True(objArray.Count == 0);
+        }
+
+        [Fact]
+        public void RemoveMethodShouldRemoveFirstOccurenceOfGivenElement()
+        {
+            object intArray = new IntArray();
+            var objArray = new List<object>() { 123, intArray, true, intArray };
+            objArray.Remove(intArray);
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(true)
+                     && objArray[2].Equals(intArray) && objArray.Count == 3);
+        }
+
+        [Fact]
+        public void RemoveAtMethodShouldRemoveElementAtGivenIndex()
+        {
+            object intArray = new IntArray();
+            var objArray = new List<object>() { 123, intArray, true };
+            objArray.RemoveAt(1);
+
+            Assert.True(objArray[0].Equals(123) && objArray[1].Equals(true)
+                     && objArray.Count == 2);
+        }
+    }
+}
