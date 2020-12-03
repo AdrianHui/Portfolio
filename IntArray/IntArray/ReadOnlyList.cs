6557c13562e891dbc9afaae7b2cac5f0cba8688f
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-03 09:33:00 +0200

    ReadOnlyList class implementation with facts

---
+++ IntArray/IntArray/ReadOnlyList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntegerArray
+{
+    class ReadOnlyList<T> : IList<T>
+    {
+        private readonly IList<T> data;
+
+        public ReadOnlyList(List<T> list)
+        {
+            data = list;
+        }
+
+        public int Count { get => data.Count; }
+
+        public bool IsReadOnly { get; } = true;
+
+        public virtual T this[int index]
+        {
+            get => data[index];
+            set
+            {
+                throw new NotSupportedException("This list is read only.");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public virtual void Add(T element)
+        {
+            throw new NotSupportedException("This list is read only.");
+        }
+
+        public bool Contains(T element)
+        {
+            return data.IndexOf(element) >= 0;
+        }
+
+        public int IndexOf(T element)
+        {
+            return data.IndexOf(element);
+        }
+
+        public virtual void Insert(int index, T element)
+        {
+            throw new NotSupportedException("This list is read only.");
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException("This list is read only.");
+        }
+
+        public bool Remove(T element)
+        {
+            throw new NotSupportedException("This list is read only.");
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotSupportedException("This list is read only.");
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            data.CopyTo(array, arrayIndex);
+        }
+    }
+}
