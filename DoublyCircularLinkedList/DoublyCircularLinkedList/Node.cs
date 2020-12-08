b305474366e622cbced73ff84c6561b9161fea9d
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-08 14:17:19 +0200

    AddLast method removed

--- DoublyCircularLinkedList/DoublyCircularLinkedList/Node.cs
+++ DoublyCircularLinkedList/DoublyCircularLinkedList/Node.cs
@@ -16,17 +16,5 @@ namespace DoublyCircularLinkedList
         public Node<T> Next { get; set; }
 
         public Node<T> Previous { get; set; }
-
-        public void AddLast(T item)
-        {
-            if (Next == null)
-            {
-                Next = new Node<T>(item);
-            }
-            else
-            {
-                Next.AddLast(item);
-            }
-        }
     }
 }
