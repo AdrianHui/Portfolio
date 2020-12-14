439ac8ee46912eacacacab303c768adf80a6e091
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-14 07:08:47 +0200

    Element class implementation

---
+++ CustomDictionary/CustomDictionary/Element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDictionary
+{
+    class Element<TKey, TValue>
+    {
+        public Element(TKey key, TValue value, int next = -1)
+        {
+            Key = key;
+            Value = value;
+            Next = next;
+        }
+
+        public TKey Key { get; set; }
+
+        public TValue Value { get; set; }
+
+        public int Next { get; set; }
+    }
+}
