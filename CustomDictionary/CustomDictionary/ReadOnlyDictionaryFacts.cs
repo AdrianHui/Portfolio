863f7e06d767997d9b7b604068abcd0d33e5b05c
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-16 08:21:22 +0200

    ReadOnlyDictionary implementation

---
+++ CustomDictionary/CustomDictionary/ReadOnlyDictionaryFacts.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace CustomDictionary.Facts
+{
+    public class ReadOnlyDictionaryFacts
+    {
+        [Fact]
+        public void KeysShouldReturnACollectionWithAllTheKeys()
+        {
+            var dict = new Dictionary<int, string>(2);
+            dict.Add(1, "a");
+            dict.Add(2, "b");
+            var readOnlyDict = new ReadOnlyDictionary<int, string>(dict);
+            var keys = readOnlyDict.Keys;
+            Assert.True(keys.Count == 2 && keys.Contains(1) && keys.Contains(2));
+        }
+    }
+}
