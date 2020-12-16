9bd209ee13aa3e210cec43d692832e5d574d3ce1
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-16 08:21:35 +0200

    exceptions added

--- CustomDictionary/CustomDictionary/Dictionary.cs
+++ CustomDictionary/CustomDictionary/Dictionary.cs
@@ -3,37 +3,78 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
 
 namespace CustomDictionary
 {
     internal class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        public Element<TKey, TValue>[] Items;
-        private readonly int[] buckets;
         private readonly int slots;
-        private int firstEmptySlot;
+        private readonly LinkedList<int> freeIndex;
+        private Element<TKey, TValue>[] elements;
+        private int[] buckets;
 
         public Dictionary(int slots)
         {
             buckets = Enumerable.Repeat(-1, slots).ToArray();
-            Items = new Element<TKey, TValue>[slots];
+            elements = new Element<TKey, TValue>[slots];
+            freeIndex = new LinkedList<int>();
             this.slots = slots;
+            GetFreeIndexes();
         }
 
-        public ICollection<TKey> Keys { get; set; }
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                List<TKey> keys = new List<TKey>();
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (elements[i] != null)
+                    {
+                        keys.Add(elements[i].Key);
+                    }
+                }
 
-        public ICollection<TValue> Values { get; set; }
+                return keys;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                List<TValue> values = new List<TValue>();
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (elements[i] != null)
+                    {
+                        values.Add(elements[i].Value);
+                    }
+                }
+
+                return values;
+            }
+        }
 
         public int Count { get; set; }
 
         public bool IsReadOnly { get; }
 
-        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public override int GetHashCode()
+        public TValue this[TKey key]
         {
-            return base.GetHashCode();
+            get
+            {
+                ValidKeyCheck(key);
+                CheckIfKeyIsInDict(key);
+                return GetValue(buckets[GetBucketIndex(key)], key);
+            }
+
+            set
+            {
+                ValidKeyCheck(key);
+                CheckIfKeyIsInDict(key);
+                SetValue(buckets[GetBucketIndex(key)], key, value);
+            }
         }
 
         public void Add(TKey key, TValue value)
@@ -43,77 +84,203 @@ namespace CustomDictionary
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
-            var hash = item.Key.GetHashCode();
-            var bucketIndex = hash < slots ? hash : hash % slots;
-            Items[firstEmptySlot] = new Element<TKey, TValue>(item.Key, item.Value, buckets[bucketIndex]);
-            buckets[bucketIndex] = firstEmptySlot;
-            firstEmptySlot++;
+            ValidKeyCheck(item.Key);
+            if (SearchBucket(buckets[GetBucketIndex(item.Key)], item.Key))
+            {
+                throw new ArgumentException(
+                    "The key you entered already exists in current collection.");
+            }
+
+            var bucketIndex = GetBucketIndex(item.Key);
+            elements[freeIndex.First.Value] =
+                new Element<TKey, TValue>(item.Key, item.Value, buckets[bucketIndex]);
+            buckets[bucketIndex] = freeIndex.First.Value;
+            freeIndex.RemoveFirst();
+            Count++;
         }
 
         public bool ContainsKey(TKey key)
         {
-            for (int i = 0; i < buckets.Length; i++)
+            ValidKeyCheck(key);
+            var bucket = GetBucketIndex(key);
+            if (bucket >= buckets.Length)
             {
-                if (i != -1 && i == key.GetHashCode())
+                return false;
+            }
+
+            return SearchBucket(buckets[bucket], key);
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return ContainsKey(item.Key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            ValidKeyCheck(key);
+            var bucket = GetBucketIndex(key);
+            var firstItem = buckets[bucket];
+            if (firstItem != -1 && elements[firstItem].Key.Equals(key))
+            {
+                freeIndex.AddFirst(firstItem);
+                buckets[bucket] = elements[firstItem].Next;
+                elements[firstItem] = default;
+                Count--;
+                return true;
+            }
+
+            for (var current = firstItem; current != -1; current = elements[current].Next)
+            {
+                var nextItem = elements[current].Next;
+                if (elements[nextItem].Key.Equals(key))
                 {
-                    return SearchBucket(buckets[i], key);
+                    freeIndex.AddFirst(nextItem);
+                    elements[current].Next = elements[nextItem].Next;
+                    elements[nextItem] = default;
+                    Count--;
+                    return true;
                 }
             }
 
             return false;
         }
 
-        public bool Remove(TKey key)
+        public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return Remove(item.Key);
         }
 
         public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
         {
-            throw new NotImplementedException();
+            ValidKeyCheck(key);
+            if (this.ContainsKey(key))
+            {
+                value = GetValue(buckets[GetBucketIndex(key)], key);
+                return true;
+            }
+
+            value = default;
+            return false;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+            Count = 0;
+            elements = new Element<TKey, TValue>[slots];
+            buckets = Enumerable.Repeat(-1, slots).ToArray();
+            GetFreeIndexes();
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
-        }
+            if (array == null)
+            {
+                throw new ArgumentNullException("array", "Cannot be null.");
+            }
+            else if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "arrayIndex", "Index was outside the bounds of the array.");
+            }
+            else if (Count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("There is not enough space in destination array.");
+            }
 
-        public bool Remove(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] != null)
+                {
+                    array[arrayIndex] =
+                    new KeyValuePair<TKey, TValue>(elements[i].Key, elements[i].Value);
+                    arrayIndex++;
+                }
+            }
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            foreach (var item in elements)
+            {
+                yield return new KeyValuePair<TKey, TValue>(item.Key, item.Value);
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private void GetFreeIndexes()
+        {
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                freeIndex.AddFirst(i);
+            }
+        }
+
+        private int GetBucketIndex(TKey key)
+        {
+            var hash = Math.Abs(key.GetHashCode());
+            return hash < slots ? hash : hash % slots;
         }
 
         private bool SearchBucket(int elemIndex, TKey key)
         {
-            for (var current = elemIndex; current != -1; current = Items[current].Next)
+            for (var current = elemIndex; current != -1; current = elements[current].Next)
             {
-                if (Items[current].Key.Equals(key))
+                if (elements[current].Key.Equals(key))
                 {
                     return true;
                 }
             }
 
             return false;
+        }
+
+        private TValue GetValue(int elemIndex, TKey key)
+        {
+            for (var current = elemIndex; current != -1; current = elements[current].Next)
+            {
+                if (elements[current].Key.Equals(key))
+                {
+                    return elements[current].Value;
+                }
+            }
+
+            return default;
+        }
+
+        private void SetValue(int elemIndex, TKey key, TValue value)
+        {
+            for (var current = elemIndex; current != -1; current = elements[current].Next)
+            {
+                if (elements[current].Key.Equals(key))
+                {
+                    elements[current].Value = value;
+                }
+            }
+        }
+
+        private void ValidKeyCheck(TKey key)
+        {
+            if (key != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException("key", "The key you entered is null.");
+        }
+
+        private void CheckIfKeyIsInDict(TKey key)
+        {
+            if (SearchBucket(buckets[GetBucketIndex(key)], key))
+            {
+                return;
+            }
+
+            throw new KeyNotFoundException("The key you entered could not be found.");
         }
     }
 }
