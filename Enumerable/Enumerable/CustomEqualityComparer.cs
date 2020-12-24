0befc604efcf3fa36a4c391e7239cdd4fe148146
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-24 15:31:43 +0200

    CustomEqualityComparer implementation

---
+++ Enumerable/Enumerable/CustomEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Enumerable
+{
+    class CustomEqualityComparer<T> : IEqualityComparer<T>
+    {
+        public bool Equals([AllowNull] T x, [AllowNull] T y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode([DisallowNull] T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
