db7da2f9fde18f878658007b0d0e9ac8c7be5b59
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-28 10:53:08 +0200

    OrderedEnumerable implemented to create a sorted collection that can be returned in OrderBy

---
+++ Enumerable/Enumerable/OrderedEnumerable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Enumerable
+{
+    class OrderedEnumerable<TSource> : IOrderedEnumerable<TSource>
+    {
+        private readonly IEnumerable<TSource> source;
+
+        public OrderedEnumerable(IEnumerable<TSource> source)
+        {
+            this.source = source;
+        }
+
+        public IOrderedEnumerable<TSource> CreateOrderedEnumerable<TKey>(
+                    Func<TSource, TKey> keySelector,
+                    IComparer<TKey> comparer,
+                    bool descending)
+        {
+            return new OrderedEnumerable<TSource>(InsertionSort(source, keySelector, comparer));
+        }
+
+        public IEnumerator<TSource> GetEnumerator()
+        {
+            foreach (var elem in source)
+            {
+                yield return elem;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<TSource> InsertionSort<TKey>(
+                    IEnumerable<TSource> source,
+                    Func<TSource, TKey> keySelector,
+                    IComparer<TKey> comparer)
+        {
+            TSource[] array = source.ToArray();
+            for (int i = 1; i < array.Length; i++)
+            {
+                var key = array[i];
+                int j;
+                for (j = i - 1;
+                     j >= 0 && comparer.Compare(keySelector(array[j]), keySelector(key)) > 0;
+                     j--)
+                {
+                    array[j + 1] = array[j];
+                }
+
+                array[j + 1] = key;
+            }
+
+            return array;
+        }
+    }
+}
