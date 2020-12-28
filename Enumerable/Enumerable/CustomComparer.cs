b5471393aab95810e3422600c18a2256469e81ec
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2020-12-28 10:52:10 +0200

    CustomComparer implementation to use with OrderBy

---
+++ Enumerable/Enumerable/CustomComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Enumerable
+{
+    class CustomComparer<T> : Comparer<T>
+        where T : IComparable<T>
+    {
+        public override int Compare([AllowNull] T x, [AllowNull] T y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
