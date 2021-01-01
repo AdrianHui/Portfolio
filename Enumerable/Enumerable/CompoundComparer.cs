461c595a314fd61c534e2c4df9f0685840bab119
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2021-01-01 16:51:46 +0200

    CompoundComparer implementation

---
+++ Enumerable/Enumerable/CompoundComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Enumerable
+{
+    class CompoundComparer<T> : IComparer<T>
+    {
+        private readonly IComparer<T> first;
+        private readonly IComparer<T> second;
+
+        public CompoundComparer(IComparer<T> first, IComparer<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public int Compare([AllowNull] T x, [AllowNull] T y)
+        {
+            int firstComparison = first.Compare(x, y);
+            return firstComparison == 0 ? second.Compare(x, y) : firstComparison;
+        }
+    }
+}
