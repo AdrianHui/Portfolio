a51f52fc5906bcae62adffeeb7e6439e2db2836a
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2021-01-01 16:53:22 +0200

    ElementComparer alows to compare two TSource elements

---
+++ Enumerable/Enumerable/ElementComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Enumerable
+{
+    class SourceComparer<TSource, TKey> : IComparer<TSource>
+    {
+        private readonly Func<TSource, TKey> keySelector;
+        private readonly IComparer<TKey> comparer;
+
+        public SourceComparer(Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            this.keySelector = keySelector;
+            this.comparer = comparer;
+        }
+
+        public int Compare([AllowNull] TSource x, [AllowNull] TSource y)
+        {
+            TKey first = keySelector(x);
+            TKey second = keySelector(y);
+            return comparer.Compare(first, second);
+        }
+    }
+}
