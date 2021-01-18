6914e7dabd6f33f68914a53e5d594336f426e63c
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2021-01-18 06:42:08 +0200

    Product class implementation

---
+++ Linq/Linq/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+#pragma warning disable CA2227 // Collection properties should be read only
+        public ICollection<Feature> Features { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+    }
+}
