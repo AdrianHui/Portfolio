8439fecbd8a4faa25fd447ab8a85141140ca08dc
AdrianHui <52374187+AdrianHui@users.noreply.github.com>
2021-02-02 07:16:11 +0200

    Central node can be displayed and the text can be changed

---
+++ MindMap/MindMap/Program.cs
@@ -0,0 +1,21 @@
+﻿using MindMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MindMapApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var centralNode = new Node();
+            while (true)
+            {
+                Console.Clear();
+                centralNode.DisplayNode();
+                centralNode.ChangeNodeText();
+            }
+        }
+    }
+}
---
+++ MindMap/MindMap2/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MindMap
+{
+    public class Node
+    {
+        public Node()
+        {
+        }
+
+        public string Text { get; private set; } = "new node";
+
+        public void DisplayNode()
+        {
+            Console.WriteLine(WrapText(Text));
+        }
+
+        public void ChangeNodeText()
+        {
+            const int low = 31;
+            const int high = 127;
+            var key = Console.ReadKey();
+            Text = key.KeyChar switch
+            {
+                '\b' => Text.Substring(0, Text.Length - 1),
+                var x when x > low && x < high => Text + x,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private string WrapText(string text)
+        {
+            return new string('-', text.Length + 2)
+                   + "\n|" + text + "|\n"
+                   + new string('-', text.Length + 2);
+        }
+    }
+}
